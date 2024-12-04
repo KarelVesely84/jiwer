9bd1af711ae8df0383298ef2b8767d88e7a6b13f
Karel Vesely <vesis84@gmail.com>
2024-12-04 14:56:23 +0100

    publish class NoTransform

--- jiwer/transforms.py
+++ jiwer/transforms.py
@@ -46,6 +46,7 @@ __all__ = [
     "AbstractTransform",
     "Compose",
     "ExpandCommonEnglishContractions",
+    "NoTransform",
     "RemoveEmptyStrings",
     "ReduceToListOfListOfWords",
     "ReduceToListOfListOfChars",
